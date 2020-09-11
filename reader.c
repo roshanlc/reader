a7e6fb2e22d54b08ac623180e217f54e47142915
Roshan Lamichhane <lamichhanersn@gmail.com>
2020-09-11 08:55:10 +0545

    Added rainbowish like rgb  color code generation, ported from lolcat

--- reader.c
+++ reader.c
@@ -12,64 +12,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define PRINTCHAR(code,ch) printf("\033[%dm%c\033[0m",code,ch) //macro function to print character with given color code as foreground
-
-
-const int codes[] = 
-  { 31, 32, 33, 34, 35, 36, 91, 92, 93, 94, 95, 96}; 
-const int codes_size = sizeof (codes) / sizeof (codes[0]);
+#include <math.h>
 
 
-int gen_rand(int,int);
-void doTheJob (char*);
-void printChar(char);
+#define PRINTRGBCHAR(r,g,b,ch) printf("\033[38;2;%d;%d;%dm%c\033[0m",r,g,b,ch) //macro function to print character with given rgb  color code as foreground
+
+const double PI = (22/7);
+
+typedef struct {
+    int r,g,b;
+}COLORS;
+
+
+COLORS return_rgb(int i)
+{
+    double f=0.1;
+    COLORS temp;
+    
+    temp.r =(int) ((sin(f* (double) i ))*127 +128);
+    temp.g =(int) ((sin(f* (double) i + 2 * PI/3))*127 +128);
+    temp.b = (int)((sin(f* (double) i +4 * PI/3))*127 +128);
+   return temp;
+}
+
 void print_usage(void);
 void print_version(void);
-
+void doTheJob(char *);
 
 int main (int argc, char *argv[])
 {
 	
-	if(argc>1)
-	{
-		if (strcmp(argv[1],"-v")==0)
-		 {
-			 print_version();
-			  exit(0);
-		}
-		else if(strcmp(argv[1],"-u")==0)
-		{
-			 print_usage();
-			  exit(0);
-		}
-	
-		FILE *fp = fopen(argv[1],"r");
-		if(fp!=NULL)
-		{
-			char x;
-			int random;
-			while((x=fgetc(fp))!=EOF)
-			{
+    COLORS check;
 
-				random = gen_rand(0,codes_size);
-				PRINTCHAR(codes[random],x);
-			}
-			
-			fclose(fp);
-		}
-		else print_usage();
-		
+
+if(argc>1)
+{
+	if (strcmp(argv[1],"-v")==0)
+	 {
+		 print_version();
+		  exit(0);
 	}
-	
+	else if(strcmp(argv[1],"-u")==0)
+	{
+		 print_usage();
+		  exit(0);
+	}
+
+	FILE *fp = fopen(argv[1],"r");
+	if(fp!=NULL)
+	{
+		char x;
+		int count=0;
+		while((x=fgetc(fp))!=EOF)
+		{
+
+            check = return_rgb(count);
+			PRINTRGBCHAR(check.r,check.g,check.b,x);
+            count++;
+		}
+		
+		fclose(fp);
+	}
 	else print_usage();
+	
+}
+
+else print_usage();
 	return 0;
 }
 
 
 void print_version (void)
 {
-	char version[] =("reader version 1.0, (c) 2020 roshan\n");
+	char version[] =("reader version 1.1, (c) 2020 roshan\n");
 	doTheJob(version);
 	exit (0);
 }
@@ -81,22 +96,14 @@ void print_usage (void)
 	exit (0);
 }
 
-int gen_rand (int low, int high)
+void doTheJob(char *str)
 {
-      int rand_num = (rand () % (high - low + 1)) + low; //generating a random number, makes use of rand()
-      return rand_num;
+    COLORS values;
+    int i=0;
+    while(str[i]!='\0')
+    {
+        values = return_rgb(i);
+        PRINTRGBCHAR(values.r,values.g,values.b,str[i]);
+        i++;
+    }
 }
-
-
-void doTheJob (char *c)
-{
-  //it takes a character and prints the character in randomly selected color
-	int x,rand;
-	for(x=0;c[x]!='\0';x++)
-	{
-		rand = gen_rand(0,codes_size);
-		PRINTCHAR(codes[rand],c[x]);
-	}
-	
-}
-
